19a9cdbb76c52a5a510f147133a89dedaa720aac
Marivaldo Pereia <marivaldo.santos42@gmail.com>
2020-03-20 19:30:58 -0300

    Inserindo dados com POST

--- src/main/java/com/marivaldopereira/workshopmongo/services/UserService.java
+++ src/main/java/com/marivaldopereira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marivaldopereira.workshopmongo.domain.User;
+import com.marivaldopereira.workshopmongo.dto.UserDTO;
 import com.marivaldopereira.workshopmongo.repository.UserRepository;
 import com.marivaldopereira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto " + id + " não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
