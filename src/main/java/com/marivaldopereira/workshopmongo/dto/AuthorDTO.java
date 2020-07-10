68d73bb4e94a6154c382083a3721930fb85c9a07
Marivaldo Pereia <marivaldo.santos42@gmail.com>
2020-07-10 12:29:25 -0300

    Projeção dos dados do Author com DTO

---
+++ src/main/java/com/marivaldopereira/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,21 @@
+package com.marivaldopereira.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import com.marivaldopereira.workshopmongo.domain.User;
+
+public class AuthorDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	
+	public AuthorDTO() {
+	}
+
+	public AuthorDTO(User obj) {
+		this.id = obj.getId();
+		this.name = obj.getName();
+	}
+}
